b8a5b09fcb843964f7483aa41c583c2895bd5917
Daniel <danielonline316@gmail.com>
2022-04-28 23:08:41 -0400

    Setting Up Dynamic Menu

--- troyers/src/views/MenuView.vue
+++ troyers/src/views/MenuView.vue
@@ -37,39 +37,44 @@
 
       <!-- <hr class="red"/> -->
     </div>
-      <img src="https://images.squarespace-cdn.com/content/v1/5d1cffbae446ee00015b3435/1565187311376-P7UDXRXRD0631XULH4ZC/red+rip-37.jpg?format=2500w" height="200" style="width: 100%; opacity: 0.7; margin-top: 60px;">
+      <img src="https://images.squarespace-cdn.com/content/v1/5d1cffbae446ee00015b3435/1565187311376-P7UDXRXRD0631XULH4ZC/red+rip-37.jpg?format=2500w" height="250" style="width: 100%; opacity: 0.7; margin-top: 60px;">
     <div class="container">
 
       <a class="jump-target" ref="Sandwiches"></a>
-      <div class="cat-container">
+      <div class="cat-container" id="subs">
         <div class="heading-container">
           <div class="menuItemTitle">
             <h2 class="vintage-heading">Sandwiches</h2>
           </div>
         </div>
         <div class="cat-content-container">
-          <div class="details-container">
+          <div class="details-container" v-if="subs">
             <div>
-              <p>All Subs:</p><span class="price">$5.49</span>
+              <p>All Subs:</p><span class="price">${{ $store.state.subPrice }}</span>
             </div>
             <div>
-              <p>Extra Meat:</p><span class="price">$1.00</span>
+              <p>Extra Meat:</p><span class="price">${{ $store.state.extraMeatPrice }}</span>
             </div>
           </div>
-          <div class="toppings-dressings-container">
+          <div class="toppings-dressings-container" v-if="subs">
             <div class="toppings">
-              <p><strong>Toppings: </strong>Lettuce, Tomatoes, Onions, Banana Peppers, Jalapeño Peppers, Pickles, Hard Boiled Eggs, Ketchup, Mustard, Mayo, Honey Mustard, Horse Radish Sauce</p>
+              <p><strong>Toppings: </strong>{{ $store.state.allToppings }}</p>
             </div>
             <div class="dressings">
-              <p><strong>Dressings: </strong>Ranch, Italian, Homemade BBQ, Homemade Poppy Seed, Homemade Sweet &amp; Sour, French </p>
+              <p><strong>Dressings: </strong>{{ $store.state.allDressings }}</p>
             </div>
           </div>
-          <div class="cat-item-container">
-            <div v-for="index in 9" :key="index" class="card border-danger">
+          <div class="cat-item-container" v-if="subs">
+            <div v-for="sub in subs" :key="sub" class="card border-danger">
               <div class="card-body">
-              <p><strong>Troyer's Special Club{{ n }}</strong></p>
-              <p>Turkey Breast, Ham, Lacy Baby Swiss Cheese. Choice of dressing &amp; Toppings</p>
+              <span class="item-title-container">
+                <p class="item-name"><strong>{{ sub.title }}</strong></p>
+              </span>
+              <p>{{ sub.toppings }}</p>
               </div>
+            </div>
+            <div  class="out-of-stock-message" v-if="!subs">
+              <p>{{ $store.state.outofSubsMessage }}</p>
             </div>
           </div>
 
@@ -77,49 +82,53 @@
       </div>
     </div>
 
-      <img src="https://images.squarespace-cdn.com/content/v1/5d1cffbae446ee00015b3435/1565187311376-P7UDXRXRD0631XULH4ZC/red+rip-37.jpg?format=2500w" height="200" style="width: 100%; opacity: 0.7; margin-top: 40px; transform: rotate(180deg);">
+      <img src="https://images.squarespace-cdn.com/content/v1/5d1cffbae446ee00015b3435/1565187311376-P7UDXRXRD0631XULH4ZC/red+rip-37.jpg?format=2500w" height="250" style="width: 100%; opacity: 0.7; margin-top: 40px; transform: rotate(180deg);">
 
     <div class="container">
       <a class="jump-target" ref="Wraps"></a>
-      <div class="cat-container">
+      <div class="cat-container" id="wraps">
         <div class="heading-container">
           <div class="menuItemTitle">
             <h2 class="vintage-heading">Wraps</h2>
           </div>
         </div>
         <div class="cat-content-container">
-          <div class="details-container">
+          <div class="details-container" v-if="wraps">
             <div>
-              <p>All Subs:</p><span class="price">$5.49</span>
+              <p>All Wraps:</p><span class="price">${{ $store.state.wrapPrice }}</span>
             </div>
             <div>
-              <p>Extra Meat:</p><span class="price">$1.00</span>
+              <p>Extra Meat:</p><span class="price">${{ $store.state.extraMeatPrice }}</span>
             </div>
           </div>
-          <div class="toppings-dressings-container">
+          <div class="toppings-dressings-container" v-if="wraps">
             <div class="toppings">
-              <p><strong>Toppings: </strong>Lettuce, Tomatoes, Onions, Banana Peppers, Jalapeño Peppers, Pickles, Hard Boiled Eggs, Ketchup, Mustard, Mayo, Honey Mustard, Horse Radish Sauce</p>
+              <p><strong>Toppings: </strong>{{ $store.state.allToppings }}</p>
             </div>
             <div class="dressings">
-              <p><strong>Dressings: </strong>Ranch, Italian, Homemade BBQ, Homemade Poppy Seed, Homemade Sweet &amp; Sour, French </p>
+              <p><strong>Dressings: </strong>{{ $store.state.allDressings }}</p>
             </div>
           </div>
-          <div class="cat-item-container">
-            <div v-for="index in 7" :key="index" class="card border-danger">
+          <div class="cat-item-container" v-if="wraps">
+            <div v-for="wrap in wraps" :key="wrap" class="card border-danger">
               <div class="card-body">
-              <p><strong>Troyer's Special Club{{ n }}</strong></p>
-              <p>Turkey Breast, Ham, Lacy Baby Swiss Cheese. Choice of dressing &amp; Toppings</p>
+              <span class="item-title-container">
+                <p class="item-name "><strong>{{ wrap.title }}</strong></p>
+              </span>
+              <p>{{ wrap.toppings }}</p>
               </div>
             </div>
           </div>
-
+          <div class="out-of-stock-message" v-else>
+            <p>{{ $store.state.outofWrapsMessage }}</p>
+          </div>
         </div>
       </div>
     </div>
 
-      <img src="https://images.squarespace-cdn.com/content/v1/5d1cffbae446ee00015b3435/1565187311376-P7UDXRXRD0631XULH4ZC/red+rip-37.jpg?format=2500w" height="200" style="width: 100%; opacity: 0.7; margin-top: 40px; transform: rotate(180deg);">
+      <img src="https://images.squarespace-cdn.com/content/v1/5d1cffbae446ee00015b3435/1565187311376-P7UDXRXRD0631XULH4ZC/red+rip-37.jpg?format=2500w" height="250" style="width: 100%; opacity: 0.7; margin-top: 40px;">
 
-    <div class="container">
+    <div class="container" id="salads">
       <a class="jump-target" ref="Salads"></a>
       <div class="cat-container">
         <div class="heading-container">
@@ -128,40 +137,55 @@
           </div>
         </div>
         <div class="cat-content-container">
-          <div class="details-container" style="margin: 60px 0 40px;">
+          <div class="details-container" v-if="salads" style="margin: 60px 0 40px;">
             <div>
-              <p>All Subs:</p><span class="price">$5.49</span>
+              <p>All Salads:</p><span class="price">${{ $store.state.saladPrice }}</span>
             </div>
-            <div class="dressings">
-              <p style="font-size: 16px; font-weight: 200;"><strong>Dressings: </strong>Ranch, Italian, Homemade BBQ, Homemade Poppy Seed, Homemade Sweet &amp; Sour, French </p>
+            <div class="dressings" style="display: flex; flex-direction: column; align-items: start; row-gap: 10px;">
+              <p style="font-size: 16px; font-weight: 200;"><strong>Dressings: </strong>{{ $store.state.saladDressings }}</p>
+              <p style="font-size: 16px; font-weight: 200;"><strong>Homemade Dressings: </strong>{{ $store.state.homemadeDressings }}</p>
             </div>
           </div>
-          <div class="cat-item-container">
-            <div v-for="index in 4" :key="index" class="card border-danger">
+          <div class="cat-item-container" v-if="salads">
+            <div v-for="salad in salads" :key="salad" class="card border-danger">
               <div class="card-body">
-              <p><strong>Troyer's Special Club{{ n }}</strong></p>
-              <p>Turkey Breast, Ham, Lacy Baby Swiss Cheese. Choice of dressing &amp; Toppings</p>
+              <span class="item-title-container">
+                <p class="item-name"><strong>{{ salad.title }}</strong></p>
+              </span>
+              <p>{{ salad.toppings }}</p>
               </div>
             </div>
           </div>
-
+            <div class="out-of-stock-message" v-else>
+              <p>{{ $store.state.outofSaladMessage }}</p>
+            </div>
         </div>
       </div>
 
     </div>
 
-      <ScrollTopArrow />
+      <ScrollTopArrow id="ScrollArrow" />
+      <OrderNowButton id="OrderNowButton" />
   </div>
 </template>
 
 <script>
 
+  import { mapState } from 'vuex'
   import ScrollTopArrow from "@/components/ScrollTopArrow.vue";
+  import OrderNowButton from "@/components/OrderNowButton.vue";
+
 
 export default {
   name: "MenuView",
   components: {
     ScrollTopArrow,
+    OrderNowButton,
+  },
+  computed:{
+    ...mapState([
+      "subs", "wraps", "salads" 
+   ]),
   },
     methods: {
     scrollToElement(element) {
@@ -176,6 +200,12 @@ export default {
 </script>
 
 <style scoped>
+#OrderNowButton.bottom-right{
+  display: block !important;
+}
+#ScrollArrow.bottom-right{
+bottom: 90px;
+}
 .jump-section span{
   display: block;
 }
@@ -208,7 +238,7 @@ export default {
   margin-bottom: -5px;
 }
 .jump-target{
-  scroll-margin-top: 180px;
+  scroll-margin-top: 120px;
 }
 .heading-container{
     border-top: 1px solid  #9e0000;
@@ -244,6 +274,12 @@ export default {
     align-items: center;
     column-gap: 20px;
   }
+   #salads .details-container div{
+    flex-basis: 50%;
+  }
+  #salads .details-container > div:first-child{
+    justify-content: flex-end;
+  }
   .details-container p{
     font-size: 20px;
     font-weight: 700;
@@ -257,6 +293,7 @@ export default {
   }
 .toppings-dressings-container{
   display: flex;
+  column-gap: 25px;
 }
 .toppings, .dressings{
   flex-basis: 50%;
@@ -272,7 +309,7 @@ export default {
   flex-basis: 30%;
   box-shadow: 2px 2px 5px 5px #d9d9d9;
   border-left-width: 5px;
-  padding: 5px 5px 5px 10px;
+  padding: 15px 5px 5px 10px;
   transition: 0.3s all;
 }
 /* .card:hover{
@@ -280,11 +317,75 @@ export default {
 } */
 .card p{
   margin-bottom: 5px;
-  font-size: 14px;
+  font-size: 15px;
 }
-.card .item-title{
+.card .item-name{
+  position: absolute;
+  top: -14px;
+  background: #ffffff;
+  padding-right: 20px;
+  font-size: 18px;
+}
+.card-body > p:last-child{
+  margin-bottom: 0;
+}
+.card .item-title-container{
+  position: relative;
+  display: block;
+  border-bottom: 2px solid #9e0000;
+  margin-bottom: 20px;
+}
+.out-of-stock-message{
+  text-align: center;
   color: #9e0000;
+  width: 100%;
+  margin-top: 60px;
 }
 
+@media only screen and (max-width: 1200px){
+  .card{
+    flex-basis: 46%;
+  }
+}
+
+@media only screen and (max-width: 768px){
+  .jump-row{
+    flex-wrap: wrap;
+    column-gap: 40px;
+    row-gap: 40px;
+  }
+  .toc{
+    flex-basis: 40%;
+    text-align: center;
+  }
+  .cat-item-container{
+    margin: 0 4%;
+    display: block;
+  }
+  .card{
+    margin: 30px 0;
+    flex-basis: 45%;
+  }
+  .details-container{
+    column-gap: 0;
+    flex-direction: column;
+    align-items: center;
+  }
+  #salads .details-container{
+    display: block;
+  }
+  #salads .details-container > div:first-child{
+    justify-content: center;
+  }
+  #salads .dressings{
+    text-align: center;  
+    align-items: unset !important;
+    margin-top: 20px;
+  }
+  .toppings-dressings-container{
+    display: block;
+    margin: 0 15px 40px;
+  }
+}
 
 </style>
