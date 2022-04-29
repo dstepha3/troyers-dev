d9156b3c027cb5c70bf53fa1cf5349ec0d8f3705
Daniel <danielonline316@gmail.com>
2022-04-28 23:08:00 -0400

    Initial

---
+++ troyers/src/components/OrderNowButton.vue
@@ -0,0 +1,59 @@
+<template>
+  <ScrollTopComponent>
+    <a class="btn btn-light" 
+       data-tooltip="Order Now"
+       data-tooltip-location="left"
+       href="tel:+13308303333">
+      <i class="fas fa-phone"></i>
+    </a>
+  </ScrollTopComponent>
+</template>
+
+<script>
+import ScrollTopComponent from "./ScrollTopComponent";
+export default {
+  components: {
+    ScrollTopComponent,
+  },
+};
+</script>
+
+<style>
+.btn {
+  border-radius: 200px !important;
+  background-color: rgba(128, 0, 0, 0.9);
+  border: 2px solid rgba(128, 0, 0, 0.9);
+  line-height: 1;
+  animation: fadeIn 0.5s;
+}
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+.fas {
+  font-size: 22px;
+  color: #ffffff;
+  padding: 10px 5px;
+}
+.fas:hover {
+  color: white;
+}
+.btn:hover {
+  border: 2px solid rgba(128, 0, 0, 0.9);
+  color: white;
+  background: rgba(180, 1, 1, 0.9);
+}
+
+.btn:hover .fas {
+  color: white;
+}
+@media screen and (max-width: 768px){
+  .fas{
+    font-size: 15px;
+  }
+}
+</style>
